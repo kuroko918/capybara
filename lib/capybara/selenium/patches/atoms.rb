db8f5d68accb483534674a693a4fcbcc93adaf29
Thomas Walpole <twalpole@gmail.com>
2019-05-11 21:18:36 -0700

    tryit

--- lib/capybara/selenium/patches/atoms.rb
+++ lib/capybara/selenium/patches/atoms.rb
@@ -1,8 +1,9 @@
 # frozen_string_literal: true
+
 require 'uglifier'
 
 module CapybaraAtoms
-  private
+private # rubocop:disable Layout/IndentationWidth
 
   def read_atom(function)
     # @atoms ||= Hash.new { |hash, key|
@@ -12,11 +13,11 @@ module CapybaraAtoms
     #     }
     #   )[0...-1]
     # }
-    @atoms ||= Hash.new { |hash, key|
+    @atoms ||= Hash.new do |hash, key|
       hash[key] = File.read(File.expand_path("../../atoms/#{key}.js", __FILE__))
-    }
+    end
     @atoms[function]
   end
 end
 
-# ::Selenium::WebDriver::Remote::Bridge.prepend CapybaraAtoms
+::Selenium::WebDriver::Remote::Bridge.prepend CapybaraAtoms
